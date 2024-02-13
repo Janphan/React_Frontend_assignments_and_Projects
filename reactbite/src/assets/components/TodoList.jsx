18233aca8e7f84b94e43179d93d4f118a6c441fd
janphan <janphan061@gmail.com>
2024-02-13 10:18:29 +0200

    update

--- reactbite/src/assets/components/TodoList.jsx
+++ reactbite/src/assets/components/TodoList.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 import { useState } from "react";
 function TodoList() {
-  const [description, setDescription] = useState("");
+  const [todo, setTodo] = useState({
+    description: "",
+    date: ""
+  });
   const [todos, setTodos] = useState([]);
 
   const handleClick = () => {
-    setTodos([...todos, description]);
+    if (todo.description && todo.date) {
+        setTodos([...todos, todo]);
+        setTodo({description: "", date: ""});
+    }
+    else {
+        alert("Type a description and date first")
+    }
   };
   return (
     <>
       <input
         placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        value={todo.description}
+        onChange={(e) => setTodo({...todo, description: e.target.value})}
       />
+      <input
+      type="date"
+      value ={todo.date}
+      onChange={(e) => setTodo({...todo, date: e.target.value})}
+      />
+      {/* sdffsj */}
+      {/* fdshjhfskdhfks */}
+      {/* sddf */}
       <button onClick={handleClick}>Add Todo</button>
+
       <table>
         <tbody>
           {todos.map((todo, index) => (
             <tr key={index}>
-              <td>{todo}</td>
+              <td>{todo.description}</td>
+              <td>{todo.date}</td>
             </tr>
           ))}
         </tbody>
