26e5a9c51655ab68ecc3836e36378ab9cf341d20
janphan <janphan061@gmail.com>
2024-03-20 17:39:57 +0200

    test icon

--- TodoList_addClear/src/assets/components/TodoList.jsx
+++ TodoList_addClear/src/assets/components/TodoList.jsx
@@ -22,6 +22,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+
 function TodoList() {
   // add light theme
   const lightTheme = createTheme({
